c47744b322faad0ed11b7bbd9e6086891cdd29b8
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-08 14:30:20 +0100

    refactor #release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,9 @@ describe DockingStation do
     it 'responds to #release_bike' do 
     expect(subject).to respond_to(:release_bike)
     end
+
+    it 'creates a new instance of the Bike class' do 
+        expect(subject.release_bike).to eq 
+    end 
+
 end
