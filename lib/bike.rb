41d830e1f28b05f47098dd01ae0f79d6c6d5a041
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-08 14:39:40 +0100

    merge #working?

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,16 +1,7 @@
 class Bike
 
-<<<<<<< HEAD
     def working?
         true
     end
     
 end
-
-=======
-    def working? # this failed initiall, because the method was returning TRUE by default, which I found surprising
-        true
-    end
-
-end
->>>>>>> d40b1881fd5d62717fe3992b555219691223a663
