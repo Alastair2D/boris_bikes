38d71cc952af127beea108b9b3e378578db77978
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-08 14:34:03 +0100

    tidy

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,7 @@
 class DockingStation 
     
     def release_bike
-<<<<<<< HEAD
         Bike.new
-=======
-        bike1 = Bike.new
->>>>>>> d40b1881fd5d62717fe3992b555219691223a663
     end
 
 end
